748c63887b0886283e1f7731e37d7a51094bb78c
Mark Gandolfo <mark@gandolfo.com.au>
2024-06-15 12:44:16 +1000

    add toggle for neovim-tree

--- init.lua
+++ init.lua
@@ -107,6 +107,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Toggle NvimTree
+vim.keymap.set('n', '<Leader>fb', ':NvimTreeToggle<CR>', { desc = 'Toggle nvim tree' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
